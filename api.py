32b3d7668ca6871c9e835aeef586a40f4242ecd0
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2022-02-06 08:14:54 +0000

    feat: add get all and create task api endpoints

---
+++ api.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from uuid import uuid4
+
+from starlette import status
+
+from schemas import CreateTaskSchema, GetTaskSchema, ListTasksSchema, Status
+from server import server
+
+TODO = []
+
+
+@server.get('/todo', response_model=ListTasksSchema)
+def get_tasks():
+    return {
+        'tasks': TODO
+    }
+
+
+@server.post('/todo', response_model=GetTaskSchema, status_code=status.HTTP_201_CREATED)
+def create_task(payload: CreateTaskSchema):
+    task = payload.dict()
+    task['id'] = uuid4()
+    task['created'] = datetime.utcnow()
+    task['priority'] = task['priority'].value
+    task['status'] = task['status'].value
+    TODO.append(task)
+    return task
