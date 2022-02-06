2db7e4730ee805650612efafac7bcbdaad761afd
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2022-02-06 09:02:58 +0000

    feat: one task endpoints (get, update, delete)

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from uuid import uuid4
+from http.client import HTTPException
+from uuid import UUID, uuid4
 
-from starlette import status
+from starlette import Response, status
 
 from schemas import CreateTaskSchema, GetTaskSchema, ListTasksSchema, Status
 from server import server
@@ -25,3 +26,34 @@ def create_task(payload: CreateTaskSchema):
     task['status'] = task['status'].value
     TODO.append(task)
     return task
+
+
+@server.get('/todo/{task_id}', response_model=GetTaskSchema)
+def get_task(task_id: UUID):
+    for task in TODO:
+        if task['id'] == task_id:
+            return task
+    raise HTTPException(
+        status_code=404, detail=f'Task with ID {task_id} was not found')
+
+
+@server.put('/todo/{task_id}', response_model=GetTaskSchema)
+def update_task(task_id: UUID, payload: CreateTaskSchema):
+    for task in TODO:
+        if task['id'] == task_id:
+            task.update(payload.dict())
+            task['status'] = task['status'].value
+            task['priority'] = task['priority'].value
+            return task
+    raise HTTPException(
+        status_code=404, detail=f'Task with ID {task_id} was not found')
+
+
+@server.delete('/todo/{task_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+def delete_task(task_id: UUID):
+    for index, task in enumerate(TODO):
+        if task['id'] == task_id:
+            TODO.pop(index)
+            return
+    raise HTTPException(
+        status_code=404, detail=f'Task with ID {task_id} was not found')
